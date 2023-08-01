d002c8405955fd8509355f58cd78e4b5e540fe40
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-08-01 10:09:34 -0400

    Changed folder structure. route modulirzed route for notes

---
+++ routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtil');
+const { v4: uuidv4 } = require('uuid');
+
+// GET Route for retrieving all the notes
+notes.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+
+notes.post('/api/notes', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding Note');
+  }
+});
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const path = require('path');
-const uuid = require('./helpers/uuid');
-const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+const api = require('./routes/index')
+
+const PORT = process.env.port || 3001;
+
+const app = express();
 
 
 app.use(express.json());
 app.use('/api', api);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
+
