28d30013236f2e07d5f598462d6394169daf7b3d
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-08-01 22:15:05 -0400

    added comments

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 
-
+// notes router
 const notesRouter = require('./notes');
-
-
 const app = express();
-
 app.use('/notes', notesRouter);
-
-
 
 module.exports = app;
